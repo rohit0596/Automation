6cca818db169fb936a0d39a904ef1673317eda74
rohit0596 <rohit.raj.0696@gmail.com>
2021-07-03 11:21:02 +0530

    third Commit

--- Automation/src/com/actitime/generics/BaseClass.java
+++ Automation/src/com/actitime/generics/BaseClass.java
@@ -5,22 +5,30 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	static {
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
 	}
 	public static WebDriver driver;
-
+	@Parameters("browser")
 	@BeforeTest 
-	public void openBrowser() {
+	public void openBrowser(String browser) {
 		Reporter.log("openBrowser",true);
-		driver = new ChromeDriver();
+		if(browser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}else if(browser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	@AfterTest 
